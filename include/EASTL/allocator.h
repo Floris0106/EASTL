46c78f43741179d6dee09bdccb4c02ecf2b7883b
Floris <mgfloris0106@gmail.com>
2024-11-25 21:23:09 +0100

    Removed last references to EA's overloaded operator new[]

--- include/EASTL/allocator.h
+++ include/EASTL/allocator.h
@@ -158,16 +158,8 @@ namespace eastl
 #ifndef EASTL_USER_DEFINED_ALLOCATOR // If the user hasn't declared that he has defined a different allocator implementation elsewhere...
 
 	EA_DISABLE_ALL_VC_WARNINGS()
-	#include <new>
+	#include <cstdlib>
 	EA_RESTORE_ALL_VC_WARNINGS()
-
-	#if !EASTL_DLL // If building a regular library and not building EASTL as a DLL...
-		// It is expected that the application define the following
-		// versions of operator new for the application. Either that or the
-		// user needs to override the implementation of the allocator class.
-		void* operator new[](size_t size, const char* pName, int flags, unsigned debugFlags, const char* file, int line);
-		void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* pName, int flags, unsigned debugFlags, const char* file, int line);
-	#endif
 
 	namespace eastl
 	{
--- test/src/TestAllocator.cpp
+++ test/src/TestAllocator.cpp
@@ -204,11 +204,6 @@ EA_RESTORE_VC_WARNING()
 
 
 
-#if EASTL_DLL
-	void* operator new[](size_t size, const char* pName, int flags, unsigned debugFlags, const char* file, int line);
-	void* operator new[](size_t size, size_t alignment, size_t alignmentOffset, const char* pName, int flags, unsigned debugFlags, const char* file, int line);
-#endif
-
 struct TestClass
 {
 	mutable int mX;
